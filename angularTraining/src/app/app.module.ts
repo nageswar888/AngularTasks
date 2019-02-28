5906a4b94f9182d9a3a95955462029a3ce6de88c
“nageswar888” <knageswar888@gmail.com>
2019-02-28 12:58:21 +0530

    angular training project

--- angularTraining/src/app/app.module.ts
+++ angularTraining/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { FilterPipeModule } from 'ngx-filter-pipe';
+import { FormsModule } from '@angular/forms';
+import { PipingPipe } from './piping.pipe';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    PipingPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FilterPipeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
